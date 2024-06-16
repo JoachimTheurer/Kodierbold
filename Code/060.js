fdcc9abffa64802c6fd21008f1f6a44a241eef44
Kodierbold <joachim.theurer@googlemail.com>
2024-06-16 19:09:48 +0200

    014 final

---
+++ Code/060.js
@@ -0,0 +1,14 @@
+function setup() {
+  createCanvas(220, 200);
+  background(230);
+  rectMode(CENTER);
+}
+
+function draw() {
+  circle(110, 140, 95);
+  circle(110,  70, 75);
+  square(110, 100, 95);
+  circle( 85,  75, 17);
+  circle(135,  75, 17);
+  square(110, 105, 15);
+}
