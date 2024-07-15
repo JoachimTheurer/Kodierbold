4fdfe1bff4add62274bf202abfba9ad60416dba5
Kodierbold <joachim.theurer@googlemail.com>
2024-07-15 12:45:55 +0200

    018 2

---
+++ Code/090.js
@@ -0,0 +1,18 @@
+function setup() {
+  createCanvas(220, 200);
+  background("brown");
+}
+/*  Kreise von außen nach innen */
+function draw() {
+  strokeWeight(25);   // Randdicke
+
+  stroke("blue");    // blauer Rand
+  circle(110, 100, 160); //  Äußerer Kreis
+
+  stroke(0, 255, 0); // grüner Rand (RGB)
+  ellipse(110, 100, 110); // Mittlerer Kreis
+
+  stroke("yellow"); // gelber Rand
+  fill(255, 0, 0); //  rote Füllung (RGB)
+  circle(110, 100, 60); // Innerer Kreis
+}
