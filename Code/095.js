2385a8dbda7ef81ce6bade099e66f75bc6414991
Kodierbold <joachim.theurer@googlemail.com>
2024-07-22 11:43:39 +0200

    018/4

--- Code/095.js
+++ Code/095.js
@@ -1,20 +1,20 @@
 function setup() {
   createCanvas(200, 200);
-  background(150, 150);
+  background('brown');
 }
 function draw() {
   /* Die fill Funktion bekommt ein 
-     viertes Argument: "alpha" */
+     viertes Argument: "Alpha" */
 
-  fill(250, 50, 50, 200); //alpha=200
+  fill(250, 50, 50, 100); //alpha=100
   square(10, 10, 80); //rotes Quadrat
 
-  fill(50, 250, 50, 150); //alpha=200
-  square(40, 40, 80); //rotes Quadrat
+  fill(50, 250, 50, 120); //alpha=120
+  square(40, 40, 80); //grünes Quadrat
 
-  fill(50, 50, 250, 100); //alpha=200
-  square(70, 70, 80); //rotes Quadrat
+  fill(50, 50, 250, 140); //alpha=140
+  square(70, 70, 80); //blaues Quadrat
 
-  fill(255, 255, 0, 50); //alpha=200
-  square(100, 100, 80); //rotes Quadrat
+  fill(255, 255, 0, 160); //alpha=160
+  square(100, 100, 80); //gelbes Quadrat
 }
