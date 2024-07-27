762e8a17f0c564814999448de8f5118505ff6421
Kodierbold <joachim.theurer@googlemail.com>
2024-07-27 20:14:53 +0200

    020/1

---
+++ Code/0110.js
@@ -0,0 +1,10 @@
+function setup() {
+  createCanvas(200, 200);
+  background("brown"); // Hintergrund Braun
+  fill("red");         // Füllfarbe Rot
+}
+
+function draw() {
+  circle(100, 100, 150);
+  fill('green');        // Füllfarbe Grün
+}
