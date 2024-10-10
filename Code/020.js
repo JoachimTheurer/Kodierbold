6d106c39780125f0cbdcd5963f9c6fc3df7b0e93
Kodierbold <joachim.theurer@googlemail.com>
2024-10-10 11:02:53 +0200

    Oktobär 10

---
+++ Code/020.js
@@ -0,0 +1,4 @@
+  createCanvas(220, 200);
+  background(202);
+  rectMode(CENTER);
+  square(110, 100, 60);
---
+++ Code/022.js
@@ -0,0 +1,8 @@
+  createCanvas(220, 200);
+  background(22);
+  circle(140, 100, 60);
+  square(110, 100, 60);
+  square(120, 110, 10);
+  square(150, 110, 10);
+  square(120, 130, 10);
+  square(150, 130, 10);
