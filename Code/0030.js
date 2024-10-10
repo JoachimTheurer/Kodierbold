c9c3b2a3ce2434b2f1bc271276c0843261441c8b
Kodierbold <joachim.theurer@googlemail.com>
2024-10-10 11:38:59 +0200

    Wackelpuppe 1

---
+++ Code/0030.js
@@ -0,0 +1,9 @@
+  createCanvas(220, 200);
+  background(220);
+  circle(110, 50, 80);
+  circle(110, 150, 110);
+  rectMode(CENTER);
+  square(110, 100, 110);
+  circle(75, 65, 20);
+  circle(145, 65, 20);
+  square(110, 100, 20);
